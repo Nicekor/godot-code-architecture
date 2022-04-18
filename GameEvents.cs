67419e11de75d6469f04235a31b2c8edac8f819d
Jose <josemorais9@protonmail.com>
2022-04-18 23:27:02 +0100

    Added food related signals

--- GameEvents.cs
+++ GameEvents.cs
@@ -5,4 +5,10 @@ public class GameEvents : Node
 {
     [Signal]
     public delegate void DialogInitiated(Dialogue dialogue);
+    [Signal]
+    public delegate void foodMousedOver(Food food);
+    [Signal]
+    public delegate void foodMousedOut();
+    [Signal]
+    public delegate void foodClicked();
 }
